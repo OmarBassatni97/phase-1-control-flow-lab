f76a0230961fbba0ba21164fe127d95d3bee1a1b
omarbassatni <omarbassatni@gmail.com>
2022-08-30 00:09:46 +0300

    finished

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feets) {
   // Write your code here!
+  if (feets <= 400) {
+    return "This one is on me!";
+  } else if (feets < 2000) {
+    return "That will be twenty bucks.";
+  } else if (feets < 2500) {
+    return "I will gladly take your thirty bucks.";
+  }
+
+  return "No can do.";
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+  let newYork = "NYC";
+  return city == newYork ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+
+  switch (tip) {
+    case "generous":
+     return "Thank you so much.";
+
+      break;
+    case "not as generous":
+      return "Thank you.";
+
+      break;
+
+    default:
+     return "Bye.";
+  }
 }
